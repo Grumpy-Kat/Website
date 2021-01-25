f989c7ab759cbd9dc192c765b43f0f9c8ec40424
Grumpy-Kat <litvin.ariela@lmghs.org>
2021-01-25 00:24:44 -0500

    Edited url mapping

--- index.php
+++ index.php
@@ -3,36 +3,36 @@
 	echo $request;
 	switch($request) {
 		case '':
-			require __DIR__ . '/html/index.php';
+			require '/html/index.php';
 			break;
 		case '/contact':
 		case '/contact.html':
 		case '/contact.php':
-			require __DIR__ . '/html/contact.php?type=feature';
+			require '/html/contact.php?type=feature';
 			break;
 		case '/faqs':
 		case '/faqs.html':
 		case '/faqs.php':
-			require __DIR__ . '/html/faqs.php';
+			require '/html/faqs.php';
 			break;
 		case '/press':
 		case '/press.html':
 		case '/press.php':
-			require __DIR__ . '/html/press.php';
+			require '/html/press.php';
 			break;
 		case '/privacy':
 		case '/privacy.html':
 		case '/privacy.php':
-			require __DIR__ . '/html/tos.php';
+			require '/html/tos.php';
 			break;
 		case '/tos':
 		case '/tos.html':
 		case '/tos.php':
-			require __DIR__ . '/html/privacy.php';
+			require '/html/privacy.php';
 			break;
-		#default:
-		#	http_response_code(404);
-		#	require __DIR__ . '/html/404.php';
-		#	break;
+		default:
+			http_response_code(404);
+			require '/html/404.php';
+			break;
 	}
 ?>
