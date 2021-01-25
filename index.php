de79fe0acd06bb44b03a5b1c35442121ae0f1a5d
Grumpy-Kat <litvin.ariela@lmghs.org>
2021-01-24 23:42:14 -0500

    Edited url mapping

--- index.php
+++ index.php
@@ -1,38 +1,38 @@
 <?php
 	$request = trim(array_slice(explode('/', $_SERVER['REQUEST_URI']), -1)[0]);
-	string redirect = '';
+	var $redirect = '';
 	switch($request) {
 		case '':
 		case '/':
 		case '/index' :
 		case '/index.html':
 		case '/index.php':
-			redirect = __DIR__ . '/html/index.php';
+			$redirect = __DIR__ . '/html/index.php';
 			break;
 		case '/contact':
 		case '/contact.html':
 		case '/contact.php':
-			redirect = __DIR__ . '/html/contact.php?type=feature';
+			$redirect = __DIR__ . '/html/contact.php?type=feature';
 			break;
 		case '/faqs':
 		case '/faqs.html':
 		case '/faqs.php':
-			redirect = __DIR__ . '/html/faqs.php';
+			$redirect = __DIR__ . '/html/faqs.php';
 			break;
 		case '/press':
 		case '/press.html':
 		case '/press.php':
-			redirect = __DIR__ . '/html/press.php';
+			$redirect = __DIR__ . '/html/press.php';
 			break;
 		case '/privacy':
 		case '/privacy.html':
 		case '/privacy.php':
-			redirect = __DIR__ . '/html/tos.php';
+			$redirect = __DIR__ . '/html/tos.php';
 			break;
 		case '/tos':
 		case '/tos.html':
 		case '/tos.php':
-			redirect = __DIR__ . '/html/privacy.php';
+			$redirect = __DIR__ . '/html/privacy.php';
 			break;
 		default:
 			http_response_code(404);
