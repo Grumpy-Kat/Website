c06cd83e188fd16b1a16bab1ab103756926acf83
Grumpy-Kat <litvin.ariela@lmghs.org>
2021-01-24 23:58:04 -0500

    Added url mapping

--- index.php
+++ index.php
@@ -1,43 +1,42 @@
 <?php
-	$request = trim(array_slice(explode('/', $_SERVER['REQUEST_URI']), -1)[0]);
-	$redirect = '';
+	$request = array_slice(explode('/', $_SERVER['REQUEST_URI']), -1)[0];
+	echo $request;
 	switch($request) {
 		case '':
 		case '/':
 		case '/index' :
 		case '/index.html':
 		case '/index.php':
-			$redirect = __DIR__ . '/html/index.php';
+			require __DIR__ . '/html/index.php';
 			break;
 		case '/contact':
 		case '/contact.html':
 		case '/contact.php':
-			$redirect = __DIR__ . '/html/contact.php?type=feature';
+			require __DIR__ . '/html/contact.php?type=feature';
 			break;
 		case '/faqs':
 		case '/faqs.html':
 		case '/faqs.php':
-			$redirect = __DIR__ . '/html/faqs.php';
+			require __DIR__ . '/html/faqs.php';
 			break;
 		case '/press':
 		case '/press.html':
 		case '/press.php':
-			$redirect = __DIR__ . '/html/press.php';
+			require __DIR__ . '/html/press.php';
 			break;
 		case '/privacy':
 		case '/privacy.html':
 		case '/privacy.php':
-			$redirect = __DIR__ . '/html/tos.php';
+			require __DIR__ . '/html/tos.php';
 			break;
 		case '/tos':
 		case '/tos.html':
 		case '/tos.php':
-			$redirect = __DIR__ . '/html/privacy.php';
+			require __DIR__ . '/html/privacy.php';
 			break;
 		default:
-			$redirect = __DIR__ . '/html/404.php';
+			http_response_code(404);
+			require __DIR__ . '/html/404.php';
 			break;
 	}
-	echo $redirect;
-	header('Location: ' . $redirect, true, 301);
 ?>
