21874c78e40ca6e3732bfcdc5ba6f5ff13283864
Grumpy-Kat <litvin.ariela@lmghs.org>
2021-01-24 23:33:14 -0500

    Added url mapping

---
+++ html/404.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<?php include "includes/head.html"; ?>
+		<link rel="stylesheet" href="styles/404.css">
+	</head>
+	<body>
+		<?php include "includes/navbar.php"; ?>
+		<div class="error">
+			<h2>An error occurred!</h2>
+		</div>
+		<?php include "includes/footer.php" ?>
+	</body>
+</html>
