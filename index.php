a307c131893398d40eb0c9b53923a00c6453ad4b
Grumpy-Kat <litvin.ariela@lmghs.org>
2021-01-25 01:03:02 -0500

    Edited url mapping

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 	$request = trim(array_slice(explode('/', $_SERVER['REQUEST_URI']), -1)[0]);
-	echo $request;
 	echo __DIR__;
 	switch($request) {
 		case '':
@@ -8,35 +7,40 @@
 		case '/index' :
 		case '/index.html':
 		case '/index.php':
-			require __DIR__ . '/html/index.php';
+		case 'app':
+		case '/app/':
+		case '/app/index' :
+		case '/app/index.html':
+		case '/app/index.php':
+			require __DIR__ . '/static/index.php';
 			break;
 		case '/contact':
 		case '/contact.html':
 		case '/contact.php':
-			require __DIR__ . '/html/contact.php?type=feature';
+			require __DIR__ . '/static/contact.php?type=feature';
 			break;
 		case '/faqs':
 		case '/faqs.html':
 		case '/faqs.php':
-			require __DIR__ . '/html/faqs.php';
+			require __DIR__ . '/static/faqs.php';
 			break;
 		case '/press':
 		case '/press.html':
 		case '/press.php':
-			require __DIR__ . '/html/press.php';
+			require __DIR__ . '/static/press.php';
 			break;
 		case '/privacy':
 		case '/privacy.html':
 		case '/privacy.php':
-			require __DIR__ . '/html/tos.php';
+			require __DIR__ . '/static/tos.php';
 			break;
 		case '/tos':
 		case '/tos.html':
 		case '/tos.php':
-			require __DIR__ . '/html/privacy.php';
+			require __DIR__ . '/static/privacy.php';
 			break;
 		default:
-			require __DIR__ . '/html/404.php';
+			require __DIR__ . '/static/index.php';
 			break;
 	}
 ?>
