8146cec9d2cd313840cc45835141cb77b3ab0797
Grumpy-Kat <litvin.ariela@lmghs.org>
2021-01-24 23:39:37 -0500

    Edited url mapping

--- index.php
+++ index.php
@@ -1,42 +1,44 @@
 <?php
 	$request = trim(array_slice(explode('/', $_SERVER['REQUEST_URI']), -1)[0]);
-	echo $request;
+	string redirect = '';
 	switch($request) {
 		case '':
 		case '/':
 		case '/index' :
 		case '/index.html':
 		case '/index.php':
-			require __DIR__ . '/html/index.php';
+			redirect = __DIR__ . '/html/index.php';
 			break;
 		case '/contact':
 		case '/contact.html':
 		case '/contact.php':
-			require __DIR__ . '/html/contact.php?type=feature';
+			redirect = __DIR__ . '/html/contact.php?type=feature';
 			break;
 		case '/faqs':
 		case '/faqs.html':
 		case '/faqs.php':
-			require __DIR__ . '/html/faqs.php';
+			redirect = __DIR__ . '/html/faqs.php';
 			break;
 		case '/press':
 		case '/press.html':
 		case '/press.php':
-			require __DIR__ . '/html/press.php';
+			redirect = __DIR__ . '/html/press.php';
 			break;
 		case '/privacy':
 		case '/privacy.html':
 		case '/privacy.php':
-			require __DIR__ . '/html/tos.php';
+			redirect = __DIR__ . '/html/tos.php';
 			break;
 		case '/tos':
 		case '/tos.html':
 		case '/tos.php':
-			require __DIR__ . '/html/privacy.php';
+			redirect = __DIR__ . '/html/privacy.php';
 			break;
 		default:
 			http_response_code(404);
 			require __DIR__ . '/html/404.php';
 			break;
 	}
+	header('Location: ' . $redirect, true, 301);
+	die();	
 ?>
