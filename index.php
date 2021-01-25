35e0341594a7c462259eaa9e0370213a67d846c6
Grumpy-Kat <litvin.ariela@lmghs.org>
2021-01-25 00:40:50 -0500

    Edited url mapping

--- index.php
+++ index.php
@@ -1,38 +1,42 @@
 <?php
-	$request = array_slice(explode('/', $_SERVER['REQUEST_URI']), -1)[0];
+	$request = trim(array_slice(explode('/', $_SERVER['REQUEST_URI']), -1)[0]);
 	echo $request;
+	echo __DIR__;
 	switch($request) {
 		case '':
-			require '/html/index.php';
+		case '/':
+		case '/index' :
+		case '/index.html':
+		case '/index.php':
+			require __DIR__ . '/html/index.php';
 			break;
 		case '/contact':
 		case '/contact.html':
 		case '/contact.php':
-			require '/html/contact.php?type=feature';
+			require __DIR__ . '/html/contact.php?type=feature';
 			break;
 		case '/faqs':
 		case '/faqs.html':
 		case '/faqs.php':
-			require '/html/faqs.php';
+			require __DIR__ . '/html/faqs.php';
 			break;
 		case '/press':
 		case '/press.html':
 		case '/press.php':
-			require '/html/press.php';
+			require __DIR__ . '/html/press.php';
 			break;
 		case '/privacy':
 		case '/privacy.html':
 		case '/privacy.php':
-			require '/html/tos.php';
+			require __DIR__ . '/html/tos.php';
 			break;
 		case '/tos':
 		case '/tos.html':
 		case '/tos.php':
-			require '/html/privacy.php';
+			require __DIR__ . '/html/privacy.php';
 			break;
 		default:
-			http_response_code(404);
-			require '/html/404.php';
+			require __DIR__ . '/html/404.php';
 			break;
 	}
 ?>
