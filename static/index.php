453f534783d8621dbb0b3d97bc5741d6379b22de
Grumpy-Kat <litvin.ariela@lmghs.org>
2021-01-25 01:12:53 -0500

    Edited url mapping

--- static/index.php
+++ static/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
-		<?php include "/includes/head.html"; ?>
+		<?php include "../includes/head.html"; ?>
 		<link rel="stylesheet" href="/styles/headerSection.css">
 		<link rel="stylesheet" href="/styles/screenSections.css">
 	</head>
 	<body>
-		<?php include "/includes/navbar.php" ?>
+		<?php include "../includes/navbar.php" ?>
 		<!--first section-->
 		<div class="container-fluid row headerSection">
 			<div class="col-md-6">
