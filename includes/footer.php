3a45516cc47bbfad0a8ba187128f09f64bf88ba8
Grumpy-Kat <litvin.ariela@lmghs.org>
2021-01-29 20:03:11 -0500

    Redid MailChimp integration

--- includes/footer.php
+++ includes/footer.php
@@ -4,9 +4,32 @@
 			<h2>Subscribe to our newsletter!</h2>
 			<h3>Stay up-to-date with updates via email.</h3>
 		</div>
-		<form class="container-fluid row mailingListForm" method="POST" action="mailchimp.php">
+		<?php
+			if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["subscribe"]) && isset($_POST["mailingList"])) {
+				$postData = array(
+					"email_address" => $_POST["mailingList"],
+					"status" => "subscribed",
+				);
+				$request = curl_init('https://us7.api.mailchimp.com/3.0/lists/'.env("mailchimpListId").'/members/');
+				curl_setopt_array(
+					$request,
+					array(
+						CURLOPT_POST => TRUE,
+						CURLOPT_RETURNTRANSFER => TRUE,
+						CURLOPT_HTTPHEADER => array(
+							'Authorization: apikey '.getenv("mailchimpKey"),
+							'Content-Type: application/json'
+						),
+						CURLOPT_POSTFIELDS => json_encode($postData)
+					)
+				);
+				$response = curl_exec($request);
+				echo $response;
+			}
+		?>
+		<form class="container-fluid row mailingListForm" method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
 			<input class="col-xs-9" type="email" id="mailingList" name="mailingList" placeholder="Email Address" required />
-			<button type="submit" class="col-xs-3 btn btn-dark shineBtn">
+			<button type="submit" name="subscribe" class="col-xs-3 btn btn-dark shineBtn">
 				<span class="shine">Subscribe</span>
 			</button>
 		</form>
